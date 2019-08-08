78b9b2c69d44b2afba54efe0463829d1b41c1776
Scream <scream870102@gmail.com>
2019-08-08 21:11:40 +0800

    修改判定時間

--- Assets/Script/NoR2252Data.cs
+++ Assets/Script/NoR2252Data.cs
@@ -4,7 +4,7 @@ namespace NoR2252.Models {
     [System.Serializable]
     public class NoR2252Data : Eccentric.Utils.TSingletonMonoBehavior<NoR2252Data> {
         public int [ ] Points = new int [ ] { 100, 80, 60, 20, 0 };
-        public float [ ] TimeGrade = new float [ ] { 0.1f, 0.2f, 0.3f, 0.35f, 0.4f };
+        public float [ ] TimeGrade = new float [ ] { 0.1f, 0.15f, 0.2f, 0.25f, 0.3f };
         public NoteColorAndSprite [ ] ColorsAndSprites = new NoteColorAndSprite [System.Enum.GetNames (typeof (ENoteType)).Length];
         public float SlideChildGradeFactor = 1.5f;
     }

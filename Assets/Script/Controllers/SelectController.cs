2c1585af11a0233d396552b90f82598d4f386031
Scream <scream870102@gmail.com>
2021-04-30 10:56:51 +0800

    Fix download music video ui

--- Assets/Script/Controllers/SelectController.cs
+++ Assets/Script/Controllers/SelectController.cs
@@ -12,7 +12,8 @@ using NoR2252.Utils;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-public class SelectController : MonoBehaviour {
+public class SelectController : MonoBehaviour
+{
     //UI REF
     [SerializeField] Text title;
     [SerializeField] Text author;
@@ -25,210 +26,237 @@ public class SelectController : MonoBehaviour {
     //store all the sheet from assetBundle
     [SerializeField] AnimationClip NextAnim;
     [SerializeField] AnimationClip PrevAnim;
-    List<GameSheet> AllSheet = new List<GameSheet> ( );
+    List<GameSheet> AllSheet = new List<GameSheet>();
     readonly float MIN_START_UP_PER = 0.05f;
     readonly int MID_SPRITE_INDEX = 2;
     //store the sheet index for sprite should display
-    List<int> cAs = new List<int> ( );
+    List<int> cAs = new List<int>();
     new AudioSource audio;
     bool bBackClicked = false;
     bool bVideoExist = false;
-    Vector2 startPos = new Vector2 ( );
-    Vector2 currentPos = new Vector2 ( );
+    Vector2 startPos = new Vector2();
+    Vector2 currentPos = new Vector2();
     int fingerId = -1;
     int direction = 0;
-    Vector2 screenSize = new Vector2 ( );
+    Vector2 screenSize = new Vector2();
     bool bSelecting = false;
 
-    void Awake ( ) {
-        audio = GetComponent<AudioSource> ( );
+    void Awake()
+    {
+        audio = GetComponent<AudioSource>();
     }
 
-    void Start ( ) {
+    void Start()
+    {
         //get all the sheet and add listener for back button onClick
-        backBtn.onClick.AddListener (OnBackClicked);
+        backBtn.onClick.AddListener(OnBackClicked);
         animation.OnAnimationFinVoid += AnimFin;
-        AllSheet.AddRange (SourceLoader.LoadAllSheets ( ));
+        AllSheet.AddRange(SourceLoader.LoadAllSheets());
         //get the best point from scoreboard
-        NoR2252Application.ScoreBoard = SourceLoader.LoadScoreBoard ( );
-        screenSize = new Vector2 (Screen.width, Screen.height);
+        NoR2252Application.ScoreBoard = SourceLoader.LoadScoreBoard();
+        screenSize = new Vector2(Screen.width, Screen.height);
         //set all the texture for sprite
         //if sheet is not exist set the value = -1
-        for (int i = 0; i < sprites.Count; i++) {
-            cAs.Add (i);
+        for (int i = 0; i < sprites.Count; i++)
+        {
+            cAs.Add(i);
             if (i < AllSheet.Count)
-                sprites [i].texture = AllSheet [i].cover;
+                sprites[i].texture = AllSheet[i].cover;
             else
-                cAs [i] = -1;
+                cAs[i] = -1;
         }
-        GetNext ( );
-        SetUI ( );
+        GetNext();
+        SetUI();
         direction = 0;
         fingerId = -1;
     }
 
     //when player tap the screen and the middle song value not equal to -4
     //set the current sheet to middle one and load game scene
-    void Tap (LeanFinger finger) {
-        audio.Play ( );
-        if (cAs [MID_SPRITE_INDEX] != -1) {
-            NoR2252Application.CurrentSheet = AllSheet [cAs [MID_SPRITE_INDEX]];
-            if (!bVideoExist) {
-                DownloadVideo (Path.GetFileName (AllSheet [cAs [MID_SPRITE_INDEX]].music));
+    void Tap(LeanFinger finger)
+    {
+        audio.Play();
+        if (cAs[MID_SPRITE_INDEX] != -1)
+        {
+            NoR2252Application.CurrentSheet = AllSheet[cAs[MID_SPRITE_INDEX]];
+            if (!bVideoExist)
+            {
+                DownloadVideo(Path.GetFileName(AllSheet[cAs[MID_SPRITE_INDEX]].music));
             }
             else if (!bBackClicked)
-                SceneManager.LoadSceneAsync ("Game");
+                SceneManager.LoadSceneAsync("Game");
         }
     }
-    void Down (LeanFinger finger) {
+    void Down(LeanFinger finger)
+    {
         startPos = finger.ScreenPosition;
         currentPos = finger.ScreenPosition;
         fingerId = finger.Index;
-        audio.Play ( );
+        audio.Play();
     }
-    void Set (LeanFinger finger) {
-        animation.Animation.Stop ( );
-        if (finger.Index == fingerId) {
+    void Set(LeanFinger finger)
+    {
+        animation.Animation.Stop();
+        if (finger.Index == fingerId)
+        {
             currentPos = finger.ScreenPosition;
             Vector2 offset = currentPos - startPos;
-            direction = offset.x > 0f?1: -1;
-            float percentage = Mathf.Abs (offset.x) / screenSize.x;
+            direction = offset.x > 0f ? 1 : -1;
+            float percentage = Mathf.Abs(offset.x) / screenSize.x;
             if (percentage <= MIN_START_UP_PER) return;
             bSelecting = true;
-            foreach (AnimationState state in animation.Animation) {
+            foreach (AnimationState state in animation.Animation)
+            {
                 if (direction == 1)
                     animation.Animation.clip = NextAnim;
                 else
                     animation.Animation.clip = PrevAnim;
                 state.time = state.length * percentage;
             }
-            animation.Animation.Play ( );
+            animation.Animation.Play();
 
         }
 
     }
-    void Up (LeanFinger finger) {
+    void Up(LeanFinger finger)
+    {
         if (bSelecting)
-            animation.Animation.Play ( );
+            animation.Animation.Play();
         bSelecting = false;
     }
 
     //if fade animation fin get the next sheet
     //and update all the ui on the scene
-    void AnimFin ( ) {
+    void AnimFin()
+    {
         if (direction == 1)
-            GetNext ( );
+            GetNext();
         else
-            GetPrev ( );
+            GetPrev();
 
-        SetUI ( );
+        SetUI();
     }
 
     // if the value is equal to -1
     //then set title and cover to error
-    void SetUI ( ) {
-        if (cAs [MID_SPRITE_INDEX] == -1) {
+    void SetUI()
+    {
+        if (cAs[MID_SPRITE_INDEX] == -1)
+        {
             title.text = "Can't get the song";
             author.text = "Error No2252X00";
         }
-        else {
-            CheckVideoExist ( );
+        else
+        {
+            CheckVideoExist();
             //try to find the bestscore pair for the sheet in the middle
             ScorePair bestPair = null;
-            title.text = AllSheet [cAs [MID_SPRITE_INDEX]].name;
-            author.text = AllSheet [cAs [MID_SPRITE_INDEX]].author;
-            bestPair = NoR2252Application.ScoreBoard.Find (AllSheet [cAs [MID_SPRITE_INDEX]].name);
+            title.text = AllSheet[cAs[MID_SPRITE_INDEX]].name;
+            author.text = AllSheet[cAs[MID_SPRITE_INDEX]].author;
+            bestPair = NoR2252Application.ScoreBoard.Find(AllSheet[cAs[MID_SPRITE_INDEX]].name);
             if (bestPair != null)
-                bestScore.text = "Best Score\n" + bestPair.Score.ToString ( );
-            else {
-                NoR2252Application.ScoreBoard.Add (AllSheet [cAs [MID_SPRITE_INDEX]].name);
+                bestScore.text = "Best Score\n" + bestPair.Score.ToString();
+            else
+            {
+                NoR2252Application.ScoreBoard.Add(AllSheet[cAs[MID_SPRITE_INDEX]].name);
                 bestScore.text = "Best Score \n0";
             }
         }
         //set all the cover for sprite 
-        for (int i = 0; i < sprites.Count; i++) {
+        for (int i = 0; i < sprites.Count; i++)
+        {
             Texture2D tex = errorTex;
-            if (cAs [i] != -1) tex = AllSheet [cAs [i]].cover;
-            sprites [i].texture = tex;
+            if (cAs[i] != -1) tex = AllSheet[cAs[i]].cover;
+            sprites[i].texture = tex;
         }
     }
 
     //calculate the index to find all the sprite value
-    void GetPrev ( ) {
-        int prev = cAs [MID_SPRITE_INDEX];
+    void GetPrev()
+    {
+        int prev = cAs[MID_SPRITE_INDEX];
         if (prev + 1 >= AllSheet.Count) prev = 0;
         else prev++;
-        cAs [MID_SPRITE_INDEX] = prev;
-        CheckRange (prev);
+        cAs[MID_SPRITE_INDEX] = prev;
+        CheckRange(prev);
     }
-    void GetNext ( ) {
-        int next = cAs [MID_SPRITE_INDEX];
+    void GetNext()
+    {
+        int next = cAs[MID_SPRITE_INDEX];
         if (next - 1 < 0) next = AllSheet.Count - 1;
         else next--;
-        cAs [MID_SPRITE_INDEX] = next;
-        CheckRange (next);
+        cAs[MID_SPRITE_INDEX] = next;
+        CheckRange(next);
     }
 
-    void CheckRange (int middle) {
-        for (int i = 0; i < sprites.Count; i++) {
+    void CheckRange(int middle)
+    {
+        for (int i = 0; i < sprites.Count; i++)
+        {
             if (i == MID_SPRITE_INDEX) continue;
-            cAs [i] = middle + MID_SPRITE_INDEX - i;
-            if (i < MID_SPRITE_INDEX) {
-                if (middle + MID_SPRITE_INDEX - i >= AllSheet.Count) {
+            cAs[i] = middle + MID_SPRITE_INDEX - i;
+            if (i < MID_SPRITE_INDEX)
+            {
+                if (middle + MID_SPRITE_INDEX - i >= AllSheet.Count)
+                {
                     int offset = middle + MID_SPRITE_INDEX - i - AllSheet.Count;
-                    cAs [i] = offset;
+                    cAs[i] = offset;
                 }
             }
-            else if (i > MID_SPRITE_INDEX) {
-                if (middle + MID_SPRITE_INDEX - i < 0) {
-                    int offset = AllSheet.Count - Mathf.Abs (middle + MID_SPRITE_INDEX - i);
-                    cAs [i] = offset;
+            else if (i > MID_SPRITE_INDEX)
+            {
+                if (middle + MID_SPRITE_INDEX - i < 0)
+                {
+                    int offset = AllSheet.Count - Mathf.Abs(middle + MID_SPRITE_INDEX - i);
+                    cAs[i] = offset;
                 }
             }
-            if (cAs [i] < 0 || cAs [i] >= AllSheet.Count) cAs [i] = -1;
+            if (cAs[i] < 0 || cAs[i] >= AllSheet.Count) cAs[i] = -1;
         }
     }
 
-    void OnBackClicked ( ) {
+    void OnBackClicked()
+    {
         bBackClicked = true;
-        SceneManager.LoadScene ("Start");
+        SceneManager.LoadScene("Start");
     }
-    void OnDisable ( ) {
+    void OnDisable()
+    {
         LeanTouch.OnFingerTap -= Tap;
         LeanTouch.OnFingerDown -= Down;
         LeanTouch.OnFingerSet -= Set;
         LeanTouch.OnFingerUp -= Up;
     }
-    void OnEnable ( ) {
+    void OnEnable()
+    {
         LeanTouch.OnFingerTap += Tap;
         LeanTouch.OnFingerDown += Down;
         LeanTouch.OnFingerSet += Set;
         LeanTouch.OnFingerUp += Up;
     }
-    void CheckVideoExist ( ) {
-        string path = Application.persistentDataPath + "/Bundle/" + Path.GetFileName (AllSheet [cAs [MID_SPRITE_INDEX]].music);
-        if (!File.Exists (path)) {
+    void CheckVideoExist()
+    {
+        string path = Application.persistentDataPath + "/Bundle/" + Path.GetFileName(AllSheet[cAs[MID_SPRITE_INDEX]].music);
+        if (!File.Exists(path))
+        {
             bVideoExist = false;
             downText.text = "Music not exist tap to download";
         }
-        else {
+        else
+        {
             bVideoExist = true;
             downText.text = "Music exist";
         }
     }
-    async void DownloadVideo (string fileName) {
+    async void DownloadVideo(string fileName)
+    {
         string netPath = FirebaseConst.VideoFolderPath + "/" + fileName;
-        Debug.Log ("Downding" + netPath);
-        Task downTask = SourceLoader.DownloadVideo (netPath, fileName);
-        if (!downTask.IsCompleted) {
-            downText.text = "Downloading ...";
-            await Task.Delay (1000 / 30);
-        }
-        //await SourceLoader.DownloadVideo (netPath, fileName);
-        Debug.Log ("Already download" + fileName);
-        if (AllSheet [cAs [MID_SPRITE_INDEX]].music == fileName) {
-            downText.text = "Music exist";
-            bVideoExist = true;
-        }
+        Debug.Log("Downding" + netPath);
+        Task downTask = SourceLoader.DownloadVideo(netPath, fileName);
+        downText.text = "Downloading ...";
+        await downTask;
+        Debug.Log("Already download" + fileName);
+        downText.text = "Music exist";
+        bVideoExist = true;
     }
 }
